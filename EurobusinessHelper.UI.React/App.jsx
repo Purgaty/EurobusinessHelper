6f99e1789339744b586021620a1d8b769eb65d04
Paweł Purgat <purgat.pawel@gmail.com>
2021-07-17 19:19:50 +0200

    Base project

---
+++ EurobusinessHelper.UI.React/App.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Cześć Krzysio! hehexd</Text>
+      <Text>It looks like it's working.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
