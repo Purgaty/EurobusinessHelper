4435fe5c383367416e68f0b061f1c2fd984d904c
Paweł Purgat <purgat.pawel@gmail.com>
2023-02-10 16:55:33 +0100

    Add missing migrations

---
+++ src/EurobusinessHelper.Infrastructure/Persistence/Migrations/20230208193221_AddStartingAccountBalance.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EurobusinessHelper.Infrastructure.Persistence.Migrations
+{
+    public partial class AddStartingAccountBalance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StartingAccountBalance",
+                table: "Games",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartingAccountBalance",
+                table: "Games");
+        }
+    }
+}
