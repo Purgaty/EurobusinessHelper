2b722ca93f3167e109908b46e20f23e29110e9d8
Paweł Purgat <purgat.pawel@gmail.com>
2023-02-11 16:24:08 +0100

    Add transfer request table

---
+++ src/EurobusinessHelper.Domain/Entities/TransferRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace EurobusinessHelper.Domain.Entities;
+
+public class TransferRequest : IEntity
+{
+    public Guid Id { get; set; }
+    public Game Game { get; set; }
+    public Account Account { get; set; }
+    public int Amount { get; set; }
+    public int ApprovalCount { get; set; }
+    public bool Approved { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public DateTime ModifiedOn { get; set; }
+}
